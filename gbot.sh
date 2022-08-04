e386b9b982f4b54a93e3b89b286bac6549a2bf22
ToF <Christophe.taudin@gmail.com>
2022-08-04 15:50:27 +0200

    maj param gbot.sh

--- gbot.sh
+++ gbot.sh
@@ -8,22 +8,22 @@
 # Default-Stop:      0 1 6
 # Short-Description: description du programme
 ### END INIT INFO
- 
-DAEMON="~/GBoT/gbot.py" #ligne de commande du programme, attention à l'extension .py.
-daemon_OPT=""  #argument à utiliser par le programme - Remplacer Utilisateur par votre nom de login
+DAEMON="/root/miniconda3/envs/gbot/bin/python" #ligne de commande du programme, attention Ã  l'extension .py.
+daemon_OPT="/root/GBoT/gbot.py"  #argument Ã  utiliser par le programme - Remplacer Utilisateur par votre nom de login
 DAEMONUSER="root" #utilisateur du programme
-daemon_NAME="gbot.py" #Nom du programme (doit être identique à l'exécutable).
+daemon_NAME="gbot.py" #Nom du programme (doit Ãªtre identique Ã  l'exÃ©cutable).
 #Attention le script est un script bash, le script ne portera donc pas l'extension .py mais .sh.
+
  
 PATH="/sbin:/bin:/usr/sbin:/usr/bin" #Ne pas toucher
  
 test -x $DAEMON || exit 0
  
 . /lib/lsb/init-functions
- 
+
 d_start () {
         log_daemon_msg "Starting system $daemon_NAME Daemon"
-	start-stop-daemon --background --name $daemon_NAME --start --quiet --chuid $DAEMONUSER --exec $DAEMON -- $daemon_OPT
+	start-stop-daemon --name $daemon_NAME --start --chuid $DAEMONUSER --exec $DAEMON -- $daemon_OPT
         log_end_msg $?
 }
  
