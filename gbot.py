ce95b0d7550e21ecf973fdcd24676c634dd9e4e6
ToF <Christophe.taudin@gmail.com>
2022-08-08 17:40:59 +0200

    DEBUG LOG >> Info

--- gbot.py
+++ gbot.py
@@ -23,7 +23,7 @@ from repeattimer import RepeatTimer
 from sessionspartiate import SessionSpartiate
 
 # Parametres 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(message)s", filemode="w")
 
 
